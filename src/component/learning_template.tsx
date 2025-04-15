ff020b4b1fa737e44bae97c678641345217dfc3a
Abdul-raham <bakareabdulrahman@gmail.com>
2025-04-15 14:37:50 +0100

    learn

--- src/component/learning_template.tsx
+++ src/component/learning_template.tsx
@@ -1,15 +1,30 @@
-import {Link } from 'react-router-dom'
-import WhiteArrow from '../assets/whitearrow.jpg'
-function Template (props){
-    return(
-        <>
-            <div className="col-md-6 col-sm-12 p-1 border rounded">
-                <img src={props.img} alt="" className='img-fluid rounded w-100' />
-                <h2 className='p-3'>{props.course}</h2>
-                <p className='p-2'>{props.note}</p>
-                <Link to={props.page} className='text-danger p-2 text-decoration-none'><b>Apply here </b><img src={WhiteArrow} alt="arrow" width={'20px'} className='m-1'/></Link>  
-            </div>
-        </>
-    )
-}
-export default Template
+import { Link } from 'react-router-dom';
+import WhiteArrow from '../assets/whitearrow.jpg';
+
+type LearningTemplateProps = {
+  img: string;
+  course: string;
+  note: string;
+  page: string;
+};
+
+const LearningTemplate = (props: LearningTemplateProps) => {
+  return (
+    <div className="col-md-6 col-sm-12 p-1 border rounded">
+      <img src={props.img} alt="" className="img-fluid rounded w-100" />
+      <h2 className="p-3">{props.course}</h2>
+      <p className="p-2">{props.note}</p>
+      <Link to={props.page} className="text-danger p-2 text-decoration-none">
+        <b>Apply here</b>
+        <img
+          src={WhiteArrow}
+          alt="arrow"
+          width="20px"
+          className="m-1"
+        />
+      </Link>
+    </div>
+  );
+};
+
+export default LearningTemplate;
--- src/component/sub_learning/sub_learning_head.tsx
+++ src/component/sub_learning/sub_learning_head.tsx
@@ -1,18 +1,29 @@
-function Head(Proos){
-    return(
-        <>
-            <div className="container-fluid bg-danger p-3">
-                <div className="row">
-                    <div className="col-md-8 col-sm-12">
-                        <h1 className="text-white ">{Proos.course}</h1>
-                        <p>{Proos.note}</p>
-                    </div>
-                    <div className="col">
-                        <img src={Proos.img} alt="image" className="img-fluid border rounded" width={"300px"} />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
-}
-export default Head
+type HeadProps = {
+    course: string;
+    note: string;
+    img: string;
+  };
+  
+  const Head = (Proos: HeadProps) => {
+    return (
+      <div className="container-fluid bg-danger p-3">
+        <div className="row">
+          <div className="col-md-8 col-sm-12">
+            <h1 className="text-white">{Proos.course}</h1>
+            <p>{Proos.note}</p>
+          </div>
+          <div className="col">
+            <img
+              src={Proos.img}
+              alt="image"
+              className="img-fluid border rounded"
+              width="300px"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Head;
+  
